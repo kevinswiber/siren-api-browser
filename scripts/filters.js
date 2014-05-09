913a9c721104d70fe3314a1d4246d76c3ca217ac
Alan Languirand <alan@13protons.com>
2014-05-09 16:36:00 -0400

    reorganized overview page

--- scripts/filters.js
+++ scripts/filters.js
@@ -63,6 +63,18 @@ sirenFilters.filter('nosub', function() {
   };
 });
 
+sirenFilters.filter('nodash', function() {
+    return function(input) {
+      return input.replace(/-/g, ' ');
+    }
+  });
+
+sirenFilters.filter('capitalize', function() {
+    return function(input, all) {
+      return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+    }
+  });
+
 sirenFilters.filter('mapurl', function() {
   return function(project) {
         var tilesUrl = {        
